731e1019639f21017ff57b1d07938d72e629cdf2
Arye Shalev <7011604+pantchox@users.noreply.github.com>
2021-01-06 20:26:08 +0200

    Update index.js
    - cache only if no error scarping open graph
    - return cache result with extra property to know it is from cache
    - change GOT user agent to whatsapp so it can parse twitter
    - validate url contains http/https

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const got = require('got');
 const cache = require('memory-cache')
 
 const metascraper = require('metascraper').load([
-  require('metascraper-author')(),
-  require('metascraper-date')(),
-  require('metascraper-description')(),
-  require('metascraper-image')(),
-  require('metascraper-logo')(),
-  require('metascraper-clearbit-logo')(),
-  require('metascraper-logo-favicon')(),
-  require('metascraper-publisher')(),
-  require('metascraper-title')(),
+	require('metascraper-author')(),
+	require('metascraper-date')(),
+	require('metascraper-description')(),
+	require('metascraper-image')(),
+	require('metascraper-logo')(),
+	require('metascraper-clearbit-logo')(),
+	require('metascraper-logo-favicon')(),
+	require('metascraper-publisher')(),
+	require('metascraper-title')(),
 	require('metascraper-url')(),
 	require('metascraper-logo-favicon')(),
 	require('metascraper-amazon')(),
@@ -29,26 +29,38 @@ const TWENTY_FOUR_HOURS = 86400000
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
 
-  const { query: { url } } = parse(req.url, true)
-  if (!url) return send(res, 401, { message: 'Please supply an URL to be scraped in the url query parameter.' })
+  let { query: { url } } = parse(req.url, true)
+  if (!url) return send(res, 400, { error: true, message: 'url query param required!' })
+
+  // validate url
+  if (!url.toLowerCase().includes('http')) {
+      // it means http or https is not included, we should add https
+      url = `https://${url}`;
+  }
 
   const cachedResult = cache.get(url)
-  if (cachedResult) return send(res, 200, cachedResult)
+  if (cachedResult) return send(res, 200, {...cachedResult, cache: true})
 
   let statusCode, data
   try {
-    const { body: html } = await got(url);
+    const { body: html } = await got(url, {
+      headers: {
+        'user-agent': 'WhatsApp/2.19.81 A', // this works for twitter as well
+      }
+    });
     data = await metascraper({ url, html })
     statusCode = 200
   } catch (err) {
-    console.log(err)
-    statusCode = 401
-    data = { message: `Scraping the open graph data from "${url}" failed.`, suggestion: 'Make sure your URL is correct and the webpage has open graph data, meta tags or twitter card data.' }
+    console.log(`Open Graph Error - ${err.toString()}`, err);
+    statusCode = 400
+    data = {error: true, message: `Connecting to "${url}" failed, Make sure your URL is correct or try again`};
+  }
+
+  res.status(statusCode).send(data);
+  // Cache results for 24 hours - not implmented
+  if (statusCode === 200) {
+  	cache.put(url, data, CACHE_IN_HOURS);
   }
   
-	cache.put(url, data, TWENTY_FOUR_HOURS)
-
   return send(res,statusCode, data)
-  // Cache results for 24 hours
-  
 }
