2ee571ab26482608d5c6e9236f3def2c996cf753
PushStaq <aryeidc@gmail>
2021-09-08 01:03:31 +0300

    fix proxy to work properly with request

--- index.js
+++ index.js
@@ -1,79 +1,81 @@
-const { parse } = require('url')
-const { send } = require('micro')
+const {parse} = require('url');
+const {send} = require('micro');
 const got = require('got');
-const cache = require('memory-cache')
+const request = require('request');
+const cache = require('memory-cache');
 
 const metascraper = require('metascraper').load([
-	require('metascraper-author')(),
-	require('metascraper-date')(),
-	require('metascraper-description')(),
-	require('metascraper-image')(),
-	require('metascraper-logo')(),
-	require('metascraper-clearbit-logo')(),
-	require('metascraper-logo-favicon')(),
-	require('metascraper-publisher')(),
-	require('metascraper-title')(),
-	require('metascraper-url')(),
-	require('metascraper-logo-favicon')(),
-	require('metascraper-amazon')(),
-	require('metascraper-youtube')(),
-	require('metascraper-soundcloud')(),
-	require('metascraper-video-provider')(),
-	require('metascraper-video')(),
-	require('metascraper-lang')()
-])
+    require('metascraper-author')(),
+    require('metascraper-date')(),
+    require('metascraper-description')(),
+    require('metascraper-image')(),
+    require('metascraper-logo')(),
+    require('metascraper-clearbit-logo')(),
+    require('metascraper-logo-favicon')(),
+    require('metascraper-publisher')(),
+    require('metascraper-title')(),
+    require('metascraper-url')(),
+    require('metascraper-amazon')(),
+    require('metascraper-youtube')(),
+    require('metascraper-soundcloud')(),
+    require('metascraper-video-provider')(),
+    require('metascraper-video')(),
+    require('metascraper-lang')(),
+]);
 
-
-const CACHE_IN_HOURS = 86400000 // 24 hours
+const CACHE_IN_HOURS = 86400000; // 24 hours
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  let { query: { url, proxy = false } } = parse(req.url, true)
-  if (!url) return send(res, 400, { error: true, message: 'url query param required!' })
+    let {
+        query: {url, proxy = false},
+    } = parse(req.url, true);
+    if (!url) return send(res, 400, {error: true, message: 'url query param required!'});
 
-  // validate url
-  if (!url.toLowerCase().includes('http')) {
-      // it means http or https is not included, we should add https
-      url = `https://${url}`;
-  }
-
-  // if proxy then pipe request
-  if (proxy) {
-    try {
-        got.stream(url, {}).pipe(res).on('finish', () => {
-          res.end();
-      }).on('error', () => {
-          return send(res, 400, { error: true, message: 'could not proxy url' })
-      });
-    } catch (e) {
-        return send(res, 400, { error: true, message: 'could not proxy url' })
+    // validate url
+    if (!url.toLowerCase().includes('http')) {
+        // it means http or https is not included, we should add https
+        url = `https://${url}`;
     }
-    return;
-  }
 
-  const cachedResult = cache.get(url)
-  if (cachedResult) return send(res, 200, {...cachedResult, cache: true})
+    if (proxy) {
+        try {
+            request({
+                url,
+                headers: {
+                    'user-agent': 'WhatsApp/2.19.81 A',
+                },
+            }).pipe(res);
+        } catch (e) {
+            console.log('catch error!', e);
+            return send(res, 400, {error: true, message: 'could not proxy url'});
+        }
+        return;
+    }
 
-  let statusCode, data
-  try {
-    const { body: html } = await got(url, {
-      headers: {
-        'user-agent': 'WhatsApp/2.19.81 A' // this works for twitter as well
-      }
-    });
-    data = await metascraper({ url, html })
-    statusCode = 200
-  } catch (err) {
-    console.log(`Open Graph Error - ${err.toString()}`, err);
-    statusCode = 400
-    data = {error: true, message: `Connecting to "${url}" failed, Make sure your URL is correct or try again`};
-  }
+    const cachedResult = cache.get(url);
+    if (cachedResult) return send(res, 200, {...cachedResult, cache: true});
 
-  // Cache results for 24 hours - not implmented
-  if (statusCode === 200) {
-  	cache.put(url, data, CACHE_IN_HOURS);
-  }
-  
-  return send(res,statusCode, data)
-}
+    let statusCode, data;
+    try {
+        const {body: html} = await got(url, {
+            headers: {
+                'user-agent': 'WhatsApp/2.19.81 A', // this works for twitter as well
+            },
+        });
+        data = await metascraper({url, html});
+        statusCode = 200;
+    } catch (err) {
+        console.log(`Open Graph Error - ${err.toString()}`, err);
+        statusCode = 400;
+        data = {error: true, message: `Connecting to "${url}" failed, Make sure your URL is correct or try again`};
+    }
+
+    // Cache results for 24 hours - not implmented
+    if (statusCode === 200) {
+        cache.put(url, data, CACHE_IN_HOURS);
+    }
+
+    return send(res, statusCode, data);
+};
